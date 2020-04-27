3dc39613adf6160e1f95bd39beb260aa9e811b5e
james davies <jamesdavies@jamess-imac.home>
2020-04-27 20:41:34 +0100

    added function in controller.js that gets stored data from db.json, combined it with new daa creating new array and rewriting it to file, also edited api .get req to cater for this

---
+++ db/controller.js
@@ -0,0 +1,36 @@
+// import notesArray from "./notesDB";
+const fs = require("fs");
+const path = require("path");
+const uuidv1 = require("uuid").v1;
+
+const writeToDB = note => {
+  try {
+    fs.writeFileSync(path.resolve(__dirname, "db.json"), JSON.stringify(note), {
+      encoding: "utf8"
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+  return true;
+};
+
+const deleteNote = id => {};
+
+const addNote = (title, text) => {
+  const existingNotes = getNotes();
+  const createdNote = { title, text, id: uuidv1() };
+  const updatedNotes = [createdNote, ...existingNotes];
+  console.log("updated notes: ", updatedNotes);
+  writeToDB(updatedNotes);
+  return createdNote;
+};
+
+const getNotes = () => {
+  const availableNotes = fs.readFileSync(path.resolve(__dirname, "db.json"));
+  return JSON.parse(availableNotes.toString("utf8"));
+};
+
+module.exports = {
+  addNote,
+  getNotes
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,29 @@
-const notesData = require("../db/notesDB");
+const addNoteToJson = require("../db/controller");
+const { addNote, getNotes, deleteNote } = require("../db/controller");
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
-    res.json(notesData);
+    const availableNotes = getNotes();
+    return res.json(availableNotes);
   });
 
   app.post("/api/notes", function(req, res) {
-    notesData.push(req.body);
-    res.json(true);
+    const { title, text } = req.body;
+
+    const createdPost = addNote(title, text);
+
+    return res.json(createdPost);
   });
 
-  app.post("/api/clear", function(req, res) {
-    notesData.length = 0;
-    res.json({ ok: true });
-  });
+  app.delete("/api/notes:id", function(req, res) {});
+
+  //   app.post("/api/notes", function(req, res) {
+  //     notesData.push(req.body);
+  //     res.json(true);
+  //   });
+
+  //   app.post("/api/clear", function(req, res) {
+  //     notesData.length = 0;
+  //     res.json({ ok: true });
+  //   });
 };
