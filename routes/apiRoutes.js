daa4cce5c4d01314bc8a6b3b02e65ad7f1cadedf
james davies <jamesdavies@jamess-imac.home>
2020-04-27 18:35:18 +0100

    added new controller.js file, added a .use method into server.js allowing us to serve the public file containing html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,15 @@
 const notesData = require("../db/notesDB");
 
 module.exports = function(app) {
-  //get req
   app.get("/api/notes", function(req, res) {
     res.json(notesData);
   });
 
-  //post req
   app.post("/api/notes", function(req, res) {
     notesData.push(req.body);
     res.json(true);
   });
 
-  //del req
   app.post("/api/clear", function(req, res) {
     notesData.length = 0;
     res.json({ ok: true });
